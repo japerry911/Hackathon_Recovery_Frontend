c6fecc54538407d6a01ac7b16393cf85bc829cea
japerry911 <jack.e.perry@outlook.com>
2020-04-28 11:40:32 -0600

    ShowPage layout finished

--- src/ShowPage.js
+++ src/ShowPage.js
@@ -1,52 +1,100 @@
-import React, { useMemo } from 'react';
+import React, { useState } from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-import { Chart } from 'react-charts';
+import { LineChart, CartesianAxis, XAxis, YAxis, Tooltip, Legend, Line } from 'recharts';
 
-const ShowPage = () => {
-    const data = useMemo(
-        () => [
-            {
-                label: 'Happiness',
-                data: [[0, 1], [1, 2], [2, 4], [3, 2], [4, 7]]
-            },
-            {
-                label: 'Sadness',
-                data: [[0, 2], [1, 3], [2, 4], [3, 5], [4, 6]]
-            },
-            {
-                label: 'Sadness',
-                data: [[0, 3], [1, 4], [2, 5], [3, 6], [4, 7]]
-            }
-        ],
-        []
-    );
+const ShowPage = ({ history }) => {
+    const [happiness, setHappiness] = useState('');
+    const [sadness, setSadness] = useState('');
+    const [anger, setAnger] = useState('');
 
-    const axes = useMemo(
-        () => [
-            { primary: true, type: 'linear', position: 'bottom' },
-            { type: 'linear', position: 'left' }
-        ],
-        []
-    );
+    const data = [
+        {
+            'date': '2020-04-21',
+            'Happiness': 1,
+            'Sadness': 2,
+            'Anger': 3
+        },
+        {
+            'date': '2020-04-22',
+            'Happiness': 4,
+            'Sadness': 5,
+            'Anger': 6
+        },
+        {
+            'date': '2020-04-23',
+            'Happiness': 7,
+            'Sadness': 8,
+            'Anger': 9
+        },
+        {
+            'date': '2020-04-24',
+            'Happiness': 10,
+            'Sadness': 9,
+            'Anger': 8
+        },
+        {
+            'date': '2020-04-25',
+            'Happiness': 7,
+            'Sadness': 6,
+            'Anger': 5
+        }
+    ];
+
+    const handleSubmit = event => {
+        event.preventDefault();
+
+        setHappiness('');
+        setSadness('');
+        setAnger('');
+
+        history.push('/');
+    };
 
     return (
         <div>
             <h1>
-                ShowPage 
+                Welcome 
             </h1>
             <div>
                 <h4>How are you feeling today?</h4>
                 <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                    <TextField type='number' label='Happiness' style={{ width: '10%' }} />
-                    <TextField type='number' label='Sadness' style={{ width: '10%' }} />
-                    <TextField type='number' label='Anger' style={{ width: '10%' }} />
-                    <Button style={{ border: '1pt solid black', marginTop: '1em', backgroundColor: 'red', opacity: 0.75 }} variant='filled'>
-                        Submit Feelings
-                    </Button>
-                    <div style={{ width: '400px', height: '300px' }}>
-                        <Chart data={data} axes={axes} />
-                    </div>
+                    <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '100%' }}>
+                        <TextField 
+                            type='number' 
+                            label='Happiness' 
+                            style={{ width: '10%' }} 
+                            value={happiness}
+                            onChange={newHappiness => setHappiness(newHappiness.target.value)}
+                        />
+                        <TextField 
+                            type='number' 
+                            label='Sadness' 
+                            style={{ width: '10%' }} 
+                            value={sadness}
+                            onChange={newSadness => setSadness(newSadness.target.value)}
+                        />
+                        <TextField 
+                            type='number' 
+                            label='Anger' 
+                            style={{ width: '10%' }} 
+                            value={anger}
+                            onChange={newAnger => setAnger(newAnger.target.value)}
+                        />
+                        <Button style={{ border: '1pt solid black', marginTop: '1em', backgroundColor: 'red', opacity: 0.75 }} type='submit'>
+                            Submit Feelings
+                        </Button>
+                    </form>
+                    <LineChart width={730} height={250} data={data} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+                        <CartesianAxis strokeDasharray='3 3' />
+                        <XAxis dataKey='date' />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Line type='monotone' dataKey='Happiness' stroke='#8884d8' />
+                        <Line type='monotone' dataKey='Sadness' stroke='#82ca9d' />
+                        <Line type='monotone' dataKey='Anger' stroke='black' />
+                    </LineChart>
                 </div>
             </div>
         </div>
